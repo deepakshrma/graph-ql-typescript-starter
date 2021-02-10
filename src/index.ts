1b81a48ef14864cd1e509f02efaa29ec50589936
xdeepakv <deepak.m.shrma@gmail.com>
2021-02-10 22:09:22 +0800

    feat: added custom apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,34 @@
-const username = "Deepak"
-console.log(`Hello ${username}`)
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+const typeDefs = `
+type User {
+  firstname: String
+  lastname: String
+  age: Int
+  address: String
+}
+type Query {
+  users:[User]
+}
+`;
+const resolvers = {
+  Query: {
+    users: () => [],
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.applyMiddleware({ app });
+
+app.get("health", (_, res) => res.send("OK"));
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+});
